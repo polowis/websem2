d4cd43a0a71127fd7a6f3dac2499ba90e4c2e7fc
polowis <tranphuochung0912@gmail.com>
2019-11-05 12:46:06 +1100

    Initial commit

---
+++ src/public/js/theme.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', checkTheme, true)
+        function changeTheme(color){
+            document.cookie = `theme=${color}`
+            $('body').css('background-color', color)
+            $('header').css('background-color', color)
+            $('section').css('background-color', color)
+            $('footer').css('background-color', color)
+        }
+        function checkTheme(){
+            if(getCookie('theme') == ""){
+                changeTheme('#191a1d')
+            }
+            else{
+                if(getCookie('theme') == "#0764FD"){
+                    changeTheme('#0764FD')
+                }
+            }
+        }
